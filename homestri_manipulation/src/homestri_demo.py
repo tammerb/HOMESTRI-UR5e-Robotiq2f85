103a3138e341fbbd3b129c285edffb53860736cb
Ian Chuang <iantc104@gmail.com>
2023-04-11 22:33:06 -0700

    finish demo

--- homestri_manipulation/src/homestri_demo.py
+++ homestri_manipulation/src/homestri_demo.py
@@ -5,7 +5,9 @@ from move_groups import Arm, Gripper
 from tf.transformations import quaternion_from_euler
 import tf
 import geometry_msgs.msg 
+import moveit_msgs.msg
 import controller_manager_msgs.srv
+import math
 
 def create_pose(x, y, z, roll, pitch, yaw, frame = ""):
     pose = geometry_msgs.msg.Pose()
@@ -21,11 +23,13 @@ def create_pose(x, y, z, roll, pitch, yaw, frame = ""):
     else:
         poseStamped = geometry_msgs.msg.PoseStamped()
         poseStamped.header.frame_id = frame
+        poseStamped.header.stamp = rospy.Time.now()
         poseStamped.pose = pose
         return poseStamped
 
 def switch_controllers(start_controllers, stop_controllers):
-    rospy.wait_for_service('/controller_manager/switch_controller')
+    rospy.wait_for_service('/controller_manager/switch_controller', timeout=3)
+    
     try:
         switch_controller = rospy.ServiceProxy(
             '/controller_manager/switch_controller', 
@@ -50,41 +54,104 @@ def zero_ft_sensor():
 if __name__ == "__main__":
 
     rospy.init_node('homestri_demo')
+    frame_pub = rospy.Publisher('/target_frame', geometry_msgs.msg.PoseStamped, queue_size=1)
 
     arm = Arm('arm')
     gripper = Gripper('gripper')
 
+    # set vel/acc scaling
+    arm.set_max_velocity_scaling_factor(1.0)
+    arm.set_max_acceleration_scaling_factor(1.0)
+
+    # open gripper slightly
     gripper.move_to_position(0.471239)
 
+    # pregrasp position
+    constraints = moveit_msgs.msg.Constraints()
+    joint_constraint = moveit_msgs.msg.JointConstraint()
+    joint_constraint.joint_name = 'wrist_1_joint'
+    joint_constraint.position = -math.pi/2
+    joint_constraint.tolerance_above = math.pi/2
+    joint_constraint.tolerance_below = math.pi/2
+    joint_constraint.weight = 10
+    constraints.joint_constraints = [joint_constraint]
+    arm.set_path_constraints(constraints)
     pose = create_pose(0.855, .105, .916, 1.5707, 0, 0)
+    arm.move_to_pose(pose, 0.15)
+    arm.clear_path_constraints()
 
-    arm.move_to_pose(pose, 0.1)
+    # grasp approach
+    arm.move_to_offset(0.15, 0, 0)
 
-    arm.move_to_offset(0.1, 0, 0)
+    # close gripper
+    gripper.move_to_target('close')
 
-    gripper.move_to_position(1.18)
+    # switch to compliance controller
+    switch_controllers(
+        ['cartesian_compliance_controller'], 
+        ['scaled_pos_joint_traj_controller']
+    )
 
-    
-    # switch_controllers(
-    #     ['cartesian_compliance_controller'], 
-    #     ['scaled_pos_joint_traj_controller']
-    # )
+    # peel build plate
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        cur_pose = arm.get_current_pose()
+        if cur_pose.pose.position.z >= 0.97:
+            break
 
-    # pub = rospy.Publisher('/target_frame', geometry_msgs.msg.PoseStamped, queue_size=1)
-    # pose = create_pose(0.10518, -.87766, 0.17319, 0, 0, 0)#, frame='base_link')
-    # arm.set_pose_reference_frame('base_link')
-    # arm.move_to_pose(pose, 0)
+        pose = create_pose(0.865, .105, 1, 1.5707, .25, 0, 'world')
+        frame_pub.publish(pose)
+        rate.sleep()
 
+    # switch back to trajectory controller
+    switch_controllers(
+        ['scaled_pos_joint_traj_controller'],
+        ['cartesian_compliance_controller']        
+    )
 
+    # raise plate
+    arm.move_to_offset(0, .1, 0)
 
-    # listener = tf.TransformListener()
+    input("PRESS ENTER TO LOWER.")
 
-    # rate = rospy.Rate(10) # 10hz
-    # while not rospy.is_shutdown():
-    #     pub.publish(pose)
-    #     rate.sleep()
+    # lower plate
+    arm.move_to_offset(0, -.1, 0)
 
+    # switch to compliance controller
+    switch_controllers(
+        ['cartesian_compliance_controller'], 
+        ['scaled_pos_joint_traj_controller']
+    )
 
-    rospy.spin()
+    # attach build plate
+    frame_pub = rospy.Publisher('/target_frame', geometry_msgs.msg.PoseStamped, queue_size=1)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        cur_pose = arm.get_current_pose()
+        if cur_pose.pose.position.z < .917:
+            break
+
+        pose = create_pose(0.855, .105, .916, 1.5707, 0, 0, 'world')
+        frame_pub.publish(pose)
+        rate.sleep()
+
+    # switch back to trajectory controller
+    switch_controllers(
+        ['scaled_pos_joint_traj_controller'],
+        ['cartesian_compliance_controller']        
+    )
+
+    # open gripper slightly
+    gripper.move_to_position(0.471239)
+
+    # grasp retreat
+    arm.move_to_offset(-0.15, 0, 0)
+
+    # go to home
+    arm.move_to_target('home')
+
+    print('FINISHED!')
+
+    # rospy.spin()
 
 
