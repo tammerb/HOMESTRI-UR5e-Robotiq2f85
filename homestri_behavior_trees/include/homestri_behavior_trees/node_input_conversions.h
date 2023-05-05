910dd90b7b56a6dd599078534500fbc5497fe052
Ian Chuang <iantc104@gmail.com>
2023-05-04 23:32:44 -0700

    added node conversions for wrench and transform

--- homestri_behavior_trees/include/homestri_behavior_trees/node_input_conversions.h
+++ homestri_behavior_trees/include/homestri_behavior_trees/node_input_conversions.h
@@ -2,6 +2,8 @@
 #define NODE_INPUT_CONVERSIONS_H
 
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Wrench.h>
+#include <geometry_msgs/Transform.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include "behaviortree_cpp_v3/behavior_tree.h"
 
@@ -40,6 +42,65 @@ namespace BT
       pose.orientation.w = quat.getW();
 
       return pose;
+    }
+    else {
+      throw RuntimeError("invalid input");
+    }
+  }
+
+  template <> inline geometry_msgs::Wrench convertFromString(StringView str)
+  {
+    // We expect real numbers separated by semicolons
+    auto parts = splitString(str, ';');
+    if (parts.size() == 6) {
+      geometry_msgs::Wrench wrench;
+      wrench.force.x = convertFromString<double>(parts[0]);
+      wrench.force.y = convertFromString<double>(parts[1]);
+      wrench.force.z = convertFromString<double>(parts[2]);
+      wrench.torque.x = convertFromString<double>(parts[3]);
+      wrench.torque.y = convertFromString<double>(parts[4]);
+      wrench.torque.z = convertFromString<double>(parts[5]);
+
+      return wrench;
+    }
+    else {
+      throw RuntimeError("invalid input");
+    }
+  }
+
+  template <> inline geometry_msgs::Transform convertFromString(StringView str)
+  {
+    // We expect real numbers separated by semicolons
+    auto parts = splitString(str, ';');
+    if (parts.size() == 7) {
+      geometry_msgs::Transform transform;
+      transform.translation.x = convertFromString<double>(parts[0]);
+      transform.translation.y = convertFromString<double>(parts[1]);
+      transform.translation.z = convertFromString<double>(parts[2]);
+      transform.rotation.x = convertFromString<double>(parts[3]);
+      transform.rotation.y = convertFromString<double>(parts[4]);
+      transform.rotation.z = convertFromString<double>(parts[5]);
+      transform.rotation.w = convertFromString<double>(parts[6]);
+
+      return transform;
+    }
+    else if (parts.size() == 6) {
+      geometry_msgs::Transform transform;
+      transform.translation.x = convertFromString<double>(parts[0]);
+      transform.translation.y = convertFromString<double>(parts[1]);
+      transform.translation.z = convertFromString<double>(parts[2]);
+
+      tf2::Quaternion quat;
+      double r = convertFromString<double>(parts[3]);
+      double p = convertFromString<double>(parts[4]);
+      double y = convertFromString<double>(parts[5]);
+      quat.setRPY( r, p, y );
+      transform.rotation.x = quat.getX();
+      transform.rotation.y = quat.getY();
+      transform.rotation.z = quat.getZ();
+      transform.rotation.w = quat.getW();
+
+      return transform;
     }
     else {
       throw RuntimeError("invalid input");
