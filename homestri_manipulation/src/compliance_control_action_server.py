dfc979b5f90bd469265fa45198eda501b0917a72
Ian Chuang <iantc104@gmail.com>
2023-05-11 23:59:52 -0700

    finished but not tested compliance control action server

--- homestri_manipulation/src/compliance_control_action_server.py
+++ homestri_manipulation/src/compliance_control_action_server.py
@@ -2,13 +2,14 @@
 
 import rospy
 import tf2_ros
+import tf2_geometry_msgs
 import tf.transformations
 import numpy as np
 import quaternion
 
 import actionlib
 
-from geometry_msgs.msg import PoseStamped, WrenchStamped, TransformStamped, Pose, Wrench, Transform
+from geometry_msgs.msg import PoseStamped, WrenchStamped, TransformStamped, Pose, Wrench, Transform, Vector3, Quaternion
 from homestri_msgs.msg import ComplianceControlAction, ComplianceControlGoal
 
 import sys
@@ -66,6 +67,58 @@ def stamp_transform(transform, child_frame_id="", frame="", time=None):
     transformStamped.transform = transform
     return transformStamped
 
+def pose_to_matrix(p):
+    # create trans and rot vectors
+    trans = [p.position.x, p.position.y, p.position.z]
+    rot = [p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w]
+    # numpy arrays to 4x4 transform matrix 
+    trans_mat = tf.transformations.translation_matrix(trans)
+    rot_mat = tf.transformations.quaternion_matrix(rot)
+
+    mat = np.dot(trans_mat, rot_mat)
+
+    return mat, trans_mat, rot_mat
+
+def matrix_to_pose(mat):
+    rot = tf.transformations.quaternion_from_matrix(mat)
+    trans = tf.transformations.translation_from_matrix(mat)
+
+    pose = create_pose(
+            trans[0],
+            trans[1],
+            trans[2],
+            rot[0],
+            rot[1],
+            rot[2],
+            rot[3]
+        )
+
+    return pose
+
+def rotational_error(q1, q2):
+    q1 = np.quaternion(q1.x, q1.y, q1.z, q1.w)
+    q2 = np.quaternion(q2.x, q2.y, q2.z, q2.w)
+
+    q_err = q1 * q2.conjugate()
+
+    angle_of_rotation = 2 * np.arccos(q_err.w)
+
+    q_err_norm = q_err / np.linalg.norm(quaternion.as_float_array(q_err))
+    axis = np.array([q_err_norm.x, q_err_norm.y, q_err_norm.z])
+
+    axis_of_rotation = angle_of_rotation * axis
+    axis_of_rotation_magnitude = np.linalg.norm(axis_of_rotation)
+
+    return angle_of_rotation, axis_of_rotation_magnitude
+
+def translational_error(t1, t2):
+    t1 = np.array((t1.x, t1.y, t1.z))
+    t2 = np.array((t2.x, t2.y, t2.z))
+
+    dist = np.linalg.norm(t1 - t2)
+
+    return dist
+
 
 class ComplianceControlActionServer(object):
     def __init__(self, name):
@@ -75,8 +128,8 @@ class ComplianceControlActionServer(object):
         self.rot_goal_tolerance = np.pi/12
 
         self.tf_timeout = rospy.Duration(3.0)
-        self.tfBuffer = tf2_ros.Buffer()
-        self.listener = tf2_ros.TransformListener(self.tfBuffer)
+        self.tf_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf_buffer)
         self.broadcaster = tf2_ros.TransformBroadcaster()
 
         self.pose_pub = rospy.Publisher(
@@ -93,30 +146,29 @@ class ComplianceControlActionServer(object):
         pose = goal.pose
         frame_id = goal.frame_id
         wrench = goal.wrench
+        mode = goal.mode
 
-        
-
-        # offset = self.__transform_pose(pose, frame_id, self.end_effector_link)
-
-
-
-
-
-        eef_pose = self.__lookup_transform(frame_id, self.end_effector_link)
-
-        print(eef_pose)
-
-        target_pose = self.__transform_pose_with_pose(pose, eef_pose)
-
-        print(target_pose)
-
-
-        target_pose = self.__transform_pose(target_pose, frame_id, self.base_link)
-
-        target_pose_stamped = stamp_pose(target_pose, frame=self.base_link)
         target_wrench_stamped = stamp_wrench(wrench, frame=self.base_link)
 
-        print(target_pose_stamped)
+        if mode == ComplianceControlGoal.MODE_OFFSET:
+            offset_pose = pose
+
+            eef_pose = self.__lookup_pose( frame_id, self.end_effector_link)
+            eef_mat, eef_trans_mat, eef_rot_mat = pose_to_matrix(eef_pose)
+            offset_mat, offset_trans_mat, offset_rot_mat = pose_to_matrix(offset_pose)
+
+            target_mat = np.dot(eef_trans_mat, np.dot(offset_mat, eef_rot_mat))
+            target_pose = matrix_to_pose(target_mat)
+            target_pose_stamped = stamp_pose(target_pose, frame=frame_id, time=rospy.Time(0))
+
+            target_pose_stamped = self.tf_buffer.transform(target_pose_stamped, self.base_link, timeout=rospy.Duration(3.0))
+
+        elif mode == ComplianceControlGoal.MODE_TARGET:
+            target_pose = pose
+
+            target_pose_stamped = stamp_pose(target_pose, frame=frame_id, time=rospy.Time(0))
+            target_pose_stamped = self.tf_buffer.transform(target_pose_stamped, self.base_link, timeout=rospy.Duration(3.0))
+
 
         # helper variables
         r = rospy.Rate(10)
@@ -126,7 +178,7 @@ class ComplianceControlActionServer(object):
         # start executing the action
         while not reached_target and not rospy.is_shutdown():
 
-            current_pose = self.__lookup_transform(self.base_link, self.end_effector_link)
+            current_pose = self.__lookup_pose(self.base_link, self.end_effector_link)
             if current_pose == None:
                 rospy.loginfo(
                     '%s: failed to read end effector frame.' % self._action_name)
@@ -152,9 +204,9 @@ class ComplianceControlActionServer(object):
                 success = False
                 break
 
-            rot_err, axis_err = self.__rotational_error(
+            rot_err, axis_err = rotational_error(
                 current_pose.orientation, target_pose.orientation)
-            trans_err = self.__translational_error(
+            trans_err = translational_error(
                 current_pose.position, target_pose.position)
 
             if rot_err < self.rot_goal_tolerance and \
@@ -180,10 +232,10 @@ class ComplianceControlActionServer(object):
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded()
 
-    def __lookup_transform(self, target_frame, source_frame):        
+    def __lookup_pose(self, target_frame, source_frame):        
         try:
-            trans = self.tfBuffer.lookup_transform(
-                target_frame, source_frame, rospy.Time(), timeout=self.tf_timeout)
+            trans = self.tf_buffer.lookup_transform(
+                target_frame, source_frame, rospy.Time(0), timeout=self.tf_timeout)
             
             pose = Pose()
             pose.position = trans.transform.translation
@@ -191,83 +243,7 @@ class ComplianceControlActionServer(object):
             return pose 
         
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            return None
-        
-    def __transform_pose(self, pose, current_frame, target_frame):
-
-
-        transform = self.__lookup_transform(target_frame, current_frame)
-
-        new_pose = self.__transform_pose_with_pose(transform, pose)
-
-
-        return new_pose
-
-
-        
-    def __transform_pose_with_pose(self, p1, p2):
-        mat1 = self.__pose_to_matrix(p1)
-        mat2 = self.__pose_to_matrix(p2)
-
-        new_mat = np.matmul(mat1, mat2)
-
-        new_p = self.__matrix_to_pose(new_mat)
-
-        return new_p
-
-    def __pose_to_matrix(self, p):
-        # create trans and rot vectors
-        trans = [p.position.x, p.position.y, p.position.z]
-        rot = [p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w]
-        # numpy arrays to 4x4 transform matrix 
-        trans_mat = tf.transformations.translation_matrix(trans)
-        rot_mat = tf.transformations.quaternion_matrix(rot)
-
-        # create a 4x4 matrix
-        mat = np.dot(trans_mat, rot_mat)
-
-        return mat
-    
-    def __matrix_to_pose(self, mat):
-        rot = tf.transformations.quaternion_from_matrix(mat)
-        trans = tf.transformations.translation_from_matrix(mat)
-
-        pose = create_pose(
-                trans[0],
-                trans[1],
-                trans[2],
-                rot[0],
-                rot[1],
-                rot[2],
-                rot[3]
-            )
-
-        return pose
-
-
-    def __rotational_error(self, q1, q2):
-        q1 = np.quaternion(q1.x, q1.y, q1.z, q1.w)
-        q2 = np.quaternion(q2.x, q2.y, q2.z, q2.w)
-
-        q_err = q1 * q2.conjugate()
-
-        angle_of_rotation = 2 * np.arccos(q_err.w)
-
-        q_err_norm = q_err / np.linalg.norm(quaternion.as_float_array(q_err))
-        axis = np.array([q_err_norm.x, q_err_norm.y, q_err_norm.z])
-
-        axis_of_rotation = angle_of_rotation * axis
-        axis_of_rotation_magnitude = np.linalg.norm(axis_of_rotation)
-
-        return angle_of_rotation, axis_of_rotation_magnitude
-
-    def __translational_error(self, t1, t2):
-        t1 = np.array((t1.x, t1.y, t1.z))
-        t2 = np.array((t2.x, t2.y, t2.z))
-
-        dist = np.linalg.norm(t1 - t2)
-
-        return dist
+            return None     
 
 if __name__ == "__main__":
     rospy.init_node('compliance_control_action_server')
@@ -275,8 +251,9 @@ if __name__ == "__main__":
 
 
     goal = ComplianceControlGoal()
-    goal.pose = create_pose(0,0,0,0.7071068, 0, 0, 0.7071068 )
+    goal.pose = create_pose(0.2,0,0.1, 0.7071068, 0, 0, 0.7071068)
     goal.frame_id = "world"
+    goal.mode = ComplianceControlGoal.MODE_OFFSET
 
     compliance_control_server.execute_cb(goal)
 
