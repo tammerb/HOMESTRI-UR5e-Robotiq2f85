1712c417636d82cde5b05fb8b38032ddc775eb1a
Ian Chuang <iantc104@gmail.com>
2023-05-18 11:53:47 -0700

    untested but finished class of cartesian control server

--- homestri_manipulation/src/cartesian_control_action_server.py
+++ homestri_manipulation/src/cartesian_control_action_server.py
@@ -5,9 +5,10 @@ import tf2_ros
 import numpy as np
 import tf2_geometry_msgs
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 
 from geometry_msgs.msg import PoseStamped, WrenchStamped, TransformStamped, Pose, Wrench, Transform, Vector3, Quaternion
-from homestri_msgs.msg import ComplianceControlAction, ComplianceControlGoal
+from homestri_msgs.msg import CartesianControlAction, CartesianControlGoal
 
 from cartesian_control_msgs.msg import (
     FollowCartesianTrajectoryAction,
@@ -20,38 +21,36 @@ import sys
 from geometry_msgs_utils import create_pose, stamp_pose, create_wrench, stamp_transform, stamp_wrench, pose_to_matrix, matrix_to_pose, rotational_error, translational_error
 
 
-class ComplianceControlActionServer(object):
+class CartesianControlActionServer(object):
     def __init__(self, name):
-        self.end_effector_link = 'gripper_tip_link'
-        self.base_link = 'base_link'
-        self.trans_goal_tolerance = 0.01
-        self.rot_goal_tolerance = np.pi/12
+        self.target_link = "gripper_tip_link"
+        self.end_effector_link = 'tool0_controller'
+        self.base_link = 'base'
+        self.controller_name = 'forward_cartesian_traj_controller'
 
         self.tf_timeout = rospy.Duration(3.0)
         self.tf_buffer = tf2_ros.Buffer()
         self.listener = tf2_ros.TransformListener(self.tf_buffer)
         self.broadcaster = tf2_ros.TransformBroadcaster()
 
-        self.pose_pub = rospy.Publisher(
-            '/target_frame', PoseStamped, queue_size=1)
-        self.wrench_pub = rospy.Publisher(
-            '/target_wrench', WrenchStamped, queue_size=1)
+        self.trajectory_client = actionlib.SimpleActionClient(
+            "{}/follow_cartesian_trajectory".format(self.controller_name),
+            FollowCartesianTrajectoryAction,
+        )
 
-        self._action_name = name
-        self._as = actionlib.SimpleActionServer(
-            self._action_name, ComplianceControlAction, execute_cb=self.execute_cb, auto_start=False)
-        self._as.start()
+        self.action_name = name
+        self.server = actionlib.SimpleActionServer(
+            self._action_name, CartesianControlAction, execute_cb=self.execute_cb, auto_start=False)
+        self.server.start()
 
     def execute_cb(self, goal):
         pose = goal.pose
         offset = goal.offset
         frame_id = goal.frame_id
-        wrench = goal.wrench
         mode = goal.mode
+        duration = goal.duration
 
-        target_wrench_stamped = stamp_wrench(wrench, frame=self.base_link)
-
-        if mode == ComplianceControlGoal.MODE_OFFSET:
+        if mode == CartesianControlGoal.MODE_OFFSET:
             eef_pose = self.__lookup_pose( frame_id, self.end_effector_link)
             _, eef_trans_mat, eef_rot_mat = pose_to_matrix(eef_pose)
             offset_mat, _, _= pose_to_matrix(offset)
@@ -62,79 +61,57 @@ class ComplianceControlActionServer(object):
 
             target_pose_stamped = self.tf_buffer.transform(target_pose_stamped, self.base_link, timeout=rospy.Duration(3.0))
 
-        elif mode == ComplianceControlGoal.MODE_TARGET:
-            pose_mat, _, _= pose_to_matrix(pose)
-            offset_mat, _, _= pose_to_matrix(offset)
+        elif mode == CartesianControlGoal.MODE_TARGET:
+            # pose_mat, _, _= pose_to_matrix(pose)
+            # offset_mat, _, _= pose_to_matrix(offset)
 
-            target_mat = np.dot(pose_mat, offset_mat)
-            target_pose = matrix_to_pose(target_mat)
-            target_pose_stamped = stamp_pose(target_pose, frame=frame_id, time=rospy.Time(0))
+            # target_mat = np.dot(pose_mat, offset_mat)
+            # target_pose = matrix_to_pose(target_mat)
+            # target_pose_stamped = stamp_pose(target_pose, frame=frame_id, time=rospy.Time(0))
+
+            # target_pose = self.__lookup_pose(self.end_effector_link, self.target_link)
+            # target_pose_stamped = stamp_pose(target_pose, frame=self.end_effector_link, time=rospy.Time(0))
             
-            target_pose_stamped = self.tf_buffer.transform(target_pose_stamped, self.base_link, timeout=rospy.Duration(3.0))
+            # target_pose_stamped = self.tf_buffer.transform(target_pose_stamped, self.base_link, timeout=rospy.Duration(3.0))
 
+            self.server.set_aborted()
+            return
 
-        # helper variables
-        r = rospy.Rate(10)
-        reached_target = False
-        success = True
+        # Wait for action server to be ready
+        if not self.trajectory_client.wait_for_server(rospy.Duration(3.0)):
+            rospy.logerr("Could not reach cartesian trajectory controller action server.")
+            self.server.set_aborted()
+            return
+        
+        # The following list are arbitrary positions
+        # Change to your own needs if desired
+        pose_list = [
+            target_pose_stamped.pose,
+        ]
+        duration_list = [duration]
+        for i, pose in enumerate(pose_list):
+            point = CartesianTrajectoryPoint()
+            point.pose = pose
+            point.time_from_start = rospy.Duration(duration_list[i])
+            goal.trajectory.points.append(point)
 
-        # start executing the action
-        while not reached_target and not rospy.is_shutdown():
-            target_pose = target_pose_stamped.pose
-            current_pose = self.__lookup_pose(self.base_link, self.end_effector_link)
-            if current_pose == None:
-                rospy.loginfo(
-                    '%s: failed to read end effector frame.' % self._action_name)
-                self._as.set_aborted()
-                success = False
-                break
+        trajectory_client.send_goal(goal)
 
-            # check that preempt has not been requested by the client
-            if self._as.is_preempt_requested():
-                rospy.loginfo('%s: Preempted' % self._action_name)
+        while (not self.server.is_preempt_requested()) and (not trajectory_client.wait_for_result(rospy.Duration(0.02))):
+            pass
 
-                current_pose_stamped = stamp_pose(
-                    current_pose, frame=self.base_link)
-
-                zero_wrench = Wrench(0, 0, 0, 0, 0, 0)
-                zero_wrench_stamped = stamp_wrench(
-                    zero_wrench, frame=self.base_link)
-
-                self.pose_pub.publish(current_pose_stamped)
-                self.wrench_pub.publish(zero_wrench_stamped)
-
-                self._as.set_preempted()
-                success = False
-                break
-
-            rot_err, axis_err = rotational_error(
-                current_pose.orientation, target_pose.orientation)
-            trans_err = translational_error(
-                current_pose.position, target_pose.position)
+        if self.server.is_preempt_requested():
+            rospy.loginfo('%s: Preempted' % self.action_name)  
+            self.trajectory_client.cancel_goal()
+            self.server.set_preempted()
+            return
+        
+        if self.trajectory_client.get_state() != GoalStatus.SUCCEEDED:
+            rospy.loginfo('%s: Failed' % self.action_name)  
+            self.server.set_aborted()
+            return
             
-            print(f"trans_err {trans_err}, rot_err {rot_err}, axis_err {axis_err}")
-
-            if rot_err < self.rot_goal_tolerance and \
-               axis_err < self.rot_goal_tolerance and \
-               trans_err < self.trans_goal_tolerance:
-                success = True
-                reached_target = True
-                break
-            
-            t = Transform()
-            t.translation = target_pose_stamped.pose.position
-            t.rotation = target_pose_stamped.pose.orientation
-            t = stamp_transform(t, child_frame_id="target_frame", frame=self.base_link)
-            self.broadcaster.sendTransform(t)
-
-            self.pose_pub.publish(target_pose_stamped)
-            self.wrench_pub.publish(target_wrench_stamped)
-
-            r.sleep()
-
-        if success:
-            rospy.loginfo('%s: Succeeded' % self._action_name)
-            self._as.set_succeeded()
+        self.server.set_succeeded()
 
     def __lookup_pose(self, target_frame, source_frame):        
         try:
