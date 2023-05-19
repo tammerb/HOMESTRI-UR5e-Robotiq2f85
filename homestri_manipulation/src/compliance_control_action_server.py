fix rotation error math

homestri_manipulation/src/compliance_control_action_server.py
@@ -16,8 +16,8 @@ class ComplianceControlActionServer(object):
     def __init__(self, name):
         self.end_effector_link = 'gripper_tip_link'
         self.base_link = 'base_link'
-        self.trans_goal_tolerance = 0.01
-        self.rot_goal_tolerance = np.pi/24
+        self.trans_goal_tolerance = 0.02
+        self.rot_goal_tolerance = np.pi/12
 
         self.tf_timeout = rospy.Duration(3.0)
         self.tf_buffer = tf2_ros.Buffer()
@@ -96,15 +96,14 @@ class ComplianceControlActionServer(object):
                 success = False
                 break
 
-            rot_err, axis_err = rotational_error(
+            rot_err = rotational_error(
                 current_pose.orientation, target_pose.orientation)
             trans_err = translational_error(
                 current_pose.position, target_pose.position)
             
             print(f"trans_err {trans_err}, rot_err {rot_err}, axis_err {axis_err}")
 
-            if (rot_err < self.rot_goal_tolerance and axis_err < self.rot_goal_tolerance) and \
-               trans_err < self.trans_goal_tolerance:
+            if rot_err < self.rot_goal_tolerance and trans_err < self.trans_goal_tolerance:
                 success = True
                 reached_target = True
                 break

homestri_manipulation/src/geometry_msgs_utils.py
@@ -91,15 +91,18 @@ def rotational_error(q1, q2):
 
     q_err = q1 * q2.conjugate()
 
-    angle_of_rotation = 2 * np.arccos(q_err.w)
+    if q_err.w < 0:
+        angle_of_rotation = 2 * np.arccos(-q_err.w)
+    else:
+        angle_of_rotation = 2 * np.arccos(q_err.w)
 
-    q_err_norm = q_err / np.linalg.norm(quaternion.as_float_array(q_err))
-    axis = np.array([q_err_norm.x, q_err_norm.y, q_err_norm.z])
+    # q_err_norm = q_err / np.linalg.norm(quaternion.as_float_array(q_err))
+    # axis = np.array([q_err_norm.x, q_err_norm.y, q_err_norm.z])
 
-    axis_of_rotation = angle_of_rotation * axis
-    axis_of_rotation_magnitude = np.linalg.norm(axis_of_rotation)
+    # axis_of_rotation = angle_of_rotation * axis
+    # axis_of_rotation_magnitude = np.linalg.norm(axis_of_rotation)
 
-    return angle_of_rotation, axis_of_rotation_magnitude
+    return angle_of_rotation #, axis_of_rotation_magnitude
 
 def translational_error(t1, t2):
     t1 = np.array((t1.x, t1.y, t1.z))