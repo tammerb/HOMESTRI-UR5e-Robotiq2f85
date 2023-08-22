dfb49cf24195796d819ff3916dc192327864800a
Ian Chuang <iantc104@gmail.com>
2023-08-22 14:55:49 -0700

    fix bugs teleop

--- catkin_ws/src/homestri-ur/homestri_bringup/scripts/xbox360_twist.py
+++ catkin_ws/src/homestri-ur/homestri_bringup/scripts/xbox360_twist.py
@@ -12,8 +12,10 @@ from onrobot_rg2ft_msgs.msg import RG2FTCommand
 
 class TeleopTwistJoy():
     def __init__(self):
-        self.twist_pub = rospy.Publisher('/joy/twist', geometry_msgs.msg.TwistStamped, queue_size=1)
-        self.gripper_pub = rospy.Publisher('/onrobot_rg2ft/command', RG2FTCommand, queue_size=1)
+
+        twist_topic = rospy.get_param("~twist_topic", "/joy/twist")
+
+        self.twist_pub = rospy.Publisher(twist_topic, geometry_msgs.msg.Twist, queue_size=1)
         self.joy_sub = rospy.Subscriber("/joy", sensor_msgs.msg.Joy, self.callback)
         
         self.joy_msg = None
@@ -25,9 +27,7 @@ class TeleopTwistJoy():
         self.joy_msg_mutex.release()
 
         if msg != None:
-            twist = geometry_msgs.msg.TwistStamped()
-            twist.header.stamp = rospy.Time.now()
-            twist.header.frame_id = "world"
+            twist = geometry_msgs.msg.Twist()
 
             axes = list(self.joy_msg.axes)
             buttons = list(self.joy_msg.buttons)
@@ -36,27 +36,15 @@ class TeleopTwistJoy():
                 if abs(axes[i]) < 0.15:
                     axes[i] = 0
 
-            linear_scale = 1
-            angular_scale = 1
+            linear_scale = 0.05
+            angular_scale = 0.05
 
-            twist.twist.linear.x = (axes[1])  * linear_scale
-            twist.twist.linear.y = (axes[0])  * linear_scale
-            twist.twist.linear.z = ((1-(axes[5]+1)/2)-(1-(axes[2]+1)/2))  * linear_scale
-            twist.twist.angular.x = (-axes[3])   * angular_scale
-            twist.twist.angular.y = (axes[4])  * angular_scale
-            twist.twist.angular.z = (buttons[2] - buttons[3])  * angular_scale
-
-
-            command = RG2FTCommand()
-            command.TargetForce = 30
-            command.Control = 0x0001
-            
-            if buttons[0] == 1:
-                command.TargetWidth = 0 # position
-                self.gripper_pub.publish(command)
-            elif buttons[1] == 1:
-                command.TargetWidth = 1000 # position
-                self.gripper_pub.publish(command)
+            twist.linear.x = (axes[1])  * linear_scale
+            twist.linear.y = (axes[0])  * linear_scale
+            twist.linear.z = ((1-(axes[5]+1)/2)-(1-(axes[2]+1)/2))  * linear_scale
+            twist.angular.x = (-axes[3])   * angular_scale
+            twist.angular.y = (axes[4])  * angular_scale
+            twist.angular.z = (buttons[2] - buttons[3])  * angular_scale
 
             self.twist_pub.publish(twist)
 
